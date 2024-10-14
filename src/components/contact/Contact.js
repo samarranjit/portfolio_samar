f244836bb41560eec47f1bcb13a5153b8012554c
Samar Ranjit <samarranjit321@gmail.com>
2024-10-13 23:04:40 -0500

    [update]: changing button type

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -144,6 +144,7 @@ const Contact = () => {
                 <button
                   onClick={handleSend}
                   className="w-full h-12 bg-[#141518] rounded-lg text-base text-gray-400 tracking-wider uppercase hover:text-white duration-300 hover:border-[1px] hover:border-designColor border-transparent"
+                  type='submit'
                 >
                   Send Message
                 </button>
